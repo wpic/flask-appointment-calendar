3088b5ac3963f51c98937cd7c202d701b7d5bd3f
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-01-14 19:35:49 +0800

    [#2] Add timezone selector, basically works.
    The builtin SelectField of wtforms didn't support optgroup element. So I
    make some hack in jinja2 template to add optgroup support.

--- fbone/appointment/forms.py
+++ fbone/appointment/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import (ValidationError, HiddenField, TextField, TextAreaField,
-                     SubmitField, DateTimeField)
+                     SubmitField, DateTimeField, SelectField)
 from wtforms.validators import Required, Length, Email
 from flask.ext.wtf.html5 import EmailField
 
@@ -12,6 +12,52 @@ EMAIL_LEN_MAX = 64
 
 CONTENT_LEN_MIN = 16
 CONTENT_LEN_MAX = 1024
+
+TIMEZONES = {
+    "TZ1": [("-8.0", "(GMT -8:00) Pacific Time (US & Canada)"),
+            ("-7.0", "(GMT -7:00) Mountain Time (US & Canada)"),
+            ("-6.0", "(GMT -6:00) Central Time (US & Canada), Mexico City"),
+            ("-5.0", "(GMT -5:00) Eastern Time (US & Canada), Bogota, Lima")],
+    "TZ2": [("8.0", "(GMT +8:00) Beijing, Perth, Singapore, Hong Kong")],
+    "TZ3": [("-12.0", "(GMT -12:00) Eniwetok, Kwajalein"),
+            ("-11.0", "(GMT -11:00) Midway Island, Samoa"),
+            ("-10.0", "(GMT -10:00) Hawaii"),
+            ("-9.0", "(GMT -9:00) Alaska"),
+            ("-8.0", "(GMT -8:00) Pacific Time (US & Canada)"),
+            ("-7.0", "(GMT -7:00) Mountain Time (US & Canada)"),
+            ("-6.0", "(GMT -6:00) Central Time (US & Canada), Mexico City"),
+            ("-5.0", "(GMT -5:00) Eastern Time (US & Canada), Bogota, Lima"),
+            ("-4.0", "(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz"),
+            ("-3.5", "(GMT -3:30) Newfoundland"),
+            ("-3.0", "(GMT -3:00) Brazil, Buenos Aires, Georgetown"),
+            ("-2.0", "(GMT -2:00) Mid-Atlantic"),
+            ("-1.0", "(GMT -1:00 hour) Azores, Cape Verde Islands"),
+            ("0.0", "(GMT) Western Europe Time, London, Lisbon, Casablanca"),
+            ("1.0", "(GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris"),
+            ("2.0", "(GMT +2:00) Kaliningrad, South Africa"),
+            ("3.0", "(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg"),
+            ("3.5", "(GMT +3:30) Tehran"),
+            ("4.0", "(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi"),
+            ("4.5", "(GMT +4:30) Kabul"),
+            ("5.0", "(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent"),
+            ("5.5", "(GMT +5:30) Bombay, Calcutta, Madras, New Delhi"),
+            ("5.75", "(GMT +5:45) Kathmandu"),
+            ("6.0", "(GMT +6:00) Almaty, Dhaka, Colombo"),
+            ("7.0", "(GMT +7:00) Bangkok, Hanoi, Jakarta"),
+            ("8.0", "(GMT +8:00) Beijing, Perth, Singapore, Hong Kong"),
+            ("9.0", "(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk"),
+            ("9.5", "(GMT +9:30) Adelaide, Darwin"),
+            ("10.0", "(GMT +10:00) Eastern Australia, Guam, Vladivostok"),
+            ("11.0", "(GMT +11:00) Magadan, Solomon Islands, New Caledonia"),
+            ("12.0", "(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka")]
+}
+
+
+class SelectOptgroupField(SelectField):
+    """
+    Monkey-patched SelectField to make it support one-level optgroup.
+    """
+    pass
 
 
 class MakeAppointmentForm(Form):
@@ -29,6 +75,8 @@ class MakeAppointmentForm(Form):
                         Length(EMAIL_LEN_MIN, EMAIL_LEN_MAX)])
     start_datetime = DateTimeField(u'Start Time')
     end_datetime = DateTimeField(u'End Time')
+    timezone = SelectOptgroupField(u'Timezone',
+                                   choices=TIMEZONES)
     content = TextAreaField(u'Content',
                             [Required(),
                              Length(CONTENT_LEN_MIN, CONTENT_LEN_MAX)])
